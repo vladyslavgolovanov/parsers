57f101ed665b41e4aa857aecfa2978ecb9f3f5a6
vladyslavgolovanov <cuzuki1@gmail.com>
2020-11-11 16:09:29 +0200

    feat:The scraper is ready

--- src/async_parser.py
+++ src/async_parser.py
@@ -33,7 +33,7 @@ from bs4 import BeautifulSoup
 
 from src.utils import read_links_txt, get_file_name_from_url
 
-links = read_links_txt('../input/images_links.txt')\
+# links = read_links_txt('../input/images_links.txt')\
 
 img_dir = '../img/async'
 
@@ -44,17 +44,17 @@ async def links_scraper(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, ssl=False) as resp:
             soup = BeautifulSoup(await resp.text(), "html.parser")
-            image = soup.findAll("link")
-            d = [i['href'] for i in image]
+            image = soup.findAll("img")
+            d = [i['src'] for i in image]
             link = []
             for i in d:
-                if (i[-4:]) == '.png':
+                if (i[-4:]) == '.jpg':
                     link.append(i)
-
-
+            yield link
 
 
 start = time.perf_counter()
+
 
 async def download(url):
     async with aiohttp.ClientSession() as session:
@@ -63,8 +63,15 @@ async def download(url):
             await f.write(await response.read())
             await f.close()
 
+
+async def app_main():
+    async for i in  links_scraper('https://wallpaperscraft.ru/'):
+        for g in i:
+            await download(g)
+
+
 loop = asyncio.get_event_loop()
-tasks = [download(link) for link in links]
-loop.run_until_complete(links_scraper("https://www.pinterest.com"))
-loop.run_until_complete(asyncio.gather(*tasks))
+# tasks = [download(link) async for link in links_scraper("https://www.pinterest.com")]
+# loop.run_until_complete(asyncio.gather(*tasks))
+loop.run_until_complete(app_main())
 print(f'Elapsed:{time.perf_counter() - start}')
